7968c7320539d97a2eedde6a2ff2e92b168ba026
Fran007 <franpru15032001@gmail.com>
2026-05-11 19:38:54 -0300

    Primer commit

---
+++ src/main/java/ort/da/hipodromo/HipodromoApplication.java
@@ -0,0 +1,13 @@
+package ort.da.hipodromo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipodromoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HipodromoApplication.class, args);
+	}
+
+}
