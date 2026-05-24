442def05a03cad9ab5c1da711385acca690205ff
Fran007 <franpru15032001@gmail.com>
2026-05-24 16:00:09 -0300

    Agregue clases Command y Commands para integracion con presentadores

---
+++ src/main/java/ort/da/hipodromo/Presentadores/Comandos/Command.java
@@ -0,0 +1,37 @@
+package ort.da.hipodromo.Presentadores.Comandos;
+
+/**
+ * Representa un comando individual que se envía al frontend.
+ * Cada comando tiene un identificador (id) y un parámetro (dato asociado).
+ * 
+ * Antes llamado "Respuesta" - renombrado para reflejar mejor su propósito
+ * como parte del patrón Command.
+ */
+public class Command {
+    private String id;
+    private Object parametro;
+
+    public Command(String id, Object parametro) {
+        this.id = id;
+        this.parametro = parametro;
+    }
+
+    public Command() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Object getParametro() {
+        return parametro;
+    }
+
+    public void setParametro(Object parametro) {
+        this.parametro = parametro;
+    }
+}
