b54f3feeea56ef694635b1d15558f14c5c25bcfb
Fran007 <franpru15032001@gmail.com>
2026-05-24 11:44:04 -0300

    Implemente fachada para coordinacion entre sistemas

--- src/main/java/ort/da/hipodromo/modelo/Fachada/Sistema.java
+++ src/main/java/ort/da/hipodromo/modelo/Fachada/Sistema.java
@@ -1,8 +1,20 @@
 package ort.da.hipodromo.modelo.Fachada;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import ort.da.hipodromo.modelo.Apuesta.Apuesta;
+import ort.da.hipodromo.modelo.Apuesta.ApuestaEnCurso;
+import ort.da.hipodromo.modelo.Apuesta.ModalidadApuesta;
+import ort.da.hipodromo.modelo.Carrera.Caballo;
+import ort.da.hipodromo.modelo.Carrera.Carrera;
+import ort.da.hipodromo.modelo.Carrera.Jornada;
+import ort.da.hipodromo.modelo.Carrera.Registro;
 import ort.da.hipodromo.modelo.Sistemas.SistemaApuestas;
 import ort.da.hipodromo.modelo.Sistemas.SistemaCarreras;
 import ort.da.hipodromo.modelo.Sistemas.SistemaUsuarios;
+import ort.da.hipodromo.modelo.Usuario.Jugador;
+import ort.da.hipodromo.modelo.Usuario.Usuario;
 
 public class Sistema {
     private static Sistema instancia;
@@ -24,5 +36,102 @@ public class Sistema {
         return instancia;
     }
 
-    
+    public Usuario login(String nombreUsuario, String password){
+        return sistemaUsuarios.login(nombreUsuario, password);
+    }
+
+    public void agregarJugador(String nombreUsuario,String password,String nombreCompleto,double saldo){
+        sistemaUsuarios.agregarJugador(nombreUsuario, password, nombreCompleto, saldo);
+    }
+
+    public void agregarAdministrador(String nombreUsuario,String password,String nombreCompleto){
+        sistemaUsuarios.agregarAdministrador(nombreUsuario, password, nombreCompleto);
+    }
+
+    public Usuario buscarUsuario(String nombreUsuario){
+        return sistemaUsuarios.buscarUsuario(nombreUsuario);
+    }
+
+    public void agregarJornada(LocalDate fecha){
+        sistemaCarreras.agregarJornada(fecha);
+    }
+
+    public Jornada buscarJornada(LocalDate fecha){
+        return sistemaCarreras.buscarJornada(fecha);
+    }
+
+    public void agregarCaballo(String nombre){
+        sistemaCarreras.agregarCaballo(nombre);
+    }
+
+    public Caballo buscarCaballo(String nombre){
+        return sistemaCarreras.buscarCaballo(nombre);
+    }
+
+    public void agregarCarrera(LocalDate fechaJornada, Carrera carrera){
+        sistemaCarreras.agregarCarrera(fechaJornada, carrera);
+    }
+
+    public Carrera buscarCarrera(LocalDate fechaJornada, int numeroCarrera){
+        return sistemaCarreras.buscarCarrera(fechaJornada, numeroCarrera);
+    }
+
+    public Registro buscarRegistro(LocalDate fechaJornada,int numeroCarrera,int numeroRegistro){
+        return sistemaCarreras.buscarRegistro(fechaJornada, numeroCarrera, numeroRegistro);
+    }
+
+    public List<Carrera> carrerasDisponiblesParaApostar(){
+        return sistemaCarreras.carrerasDisponiblesParaApostar();
+    }
+
+    public Jornada jornadaActual(){
+        return sistemaCarreras.jornadaActual();
+    }
+
+    public Jornada jornadaSiguiente(LocalDate fechaActual){
+        return sistemaCarreras.jornadaSiguiente(fechaActual);
+    }
+
+    public Jornada jornadaAnterior(LocalDate fechaActual){
+        return sistemaCarreras.jornadaAnterior(fechaActual);
+    }
+
+    public void abrirCarrera(LocalDate fechaJornada, int numeroCarrera){
+        sistemaCarreras.abrirCarrera(fechaJornada, numeroCarrera);
+    }
+
+    public void cerrarCarrera(LocalDate fechaJornada, int numeroCarrera){
+        sistemaCarreras.cerrarCarrera(fechaJornada, numeroCarrera, sistemaApuestas.getComision());
+    }
+
+    public void finalizarCarrera(LocalDate fechaJornada, int numeroCarrera,int numeroRegistroGanador){
+        sistemaCarreras.finalizarCarrera(fechaJornada, numeroCarrera, numeroRegistroGanador);
+    }
+
+    public void agregarModalidad(ModalidadApuesta modalidad){
+        sistemaApuestas.agregarModalidad(modalidad);
+    }
+
+    public ModalidadApuesta buscarModalidad(String nombre){
+        return sistemaApuestas.buscarModalidad(nombre);
+    }
+
+    public ApuestaEnCurso crearApuestaEnCurso(String nombreUsuario,LocalDate fechaJornada, int numeroCarrera, int numeroRegistro, String nombreModalidad, double monto){
+        Usuario usuario = this.buscarUsuario(nombreUsuario);
+        Jugador jugador = (Jugador) usuario;
+
+        Carrera carrera = this.buscarCarrera(fechaJornada, numeroCarrera);
+        Registro registro = this.buscarRegistro(fechaJornada, numeroCarrera, numeroRegistro);
+        ModalidadApuesta modalidad = this.buscarModalidad(nombreModalidad);
+
+        return sistemaApuestas.crearApuestaEnCurso(jugador, carrera, registro, modalidad, monto);
+    }
+
+    public Apuesta confirmarApuesta(ApuestaEnCurso apuestaEnCurso, String password){
+        return sistemaApuestas.confirmarApuesta(apuestaEnCurso, password);
+    }
+
+    public void cancelarApuesta(ApuestaEnCurso apuestaEnCurso){
+        sistemaApuestas.cancelarApuesta(apuestaEnCurso);
+    }
 }
